7c11b0923570d65b6d7cec72de40e1e7c9f8f373
Victoria Cheng <victoria.cheng15@gmail.com>
2022-02-21 12:50:13 -0500

    add day 1 section and file

---
+++ coffee-background-picker/js/main.js
@@ -0,0 +1,39 @@
+document.querySelector('li:first-child').onclick = first
+document.querySelector('li:nth-child(2)').onclick = second
+document.querySelector('li:nth-child(3)').onclick = third
+document.querySelector('li:nth-child(4)').onclick = fourth
+document.querySelector('li:nth-child(5)').onclick = fifth
+
+function changeImage(link) {
+  document.querySelector('[data-image]').innerHTML = ''
+  const img = document.createElement('img')
+  img.src = link
+  img.alt = 'coffee!!'
+  document.querySelector('[data-image]').appendChild(img)
+}
+
+function first() {
+  document.body.style.backgroundColor = '#38220f'
+  changeImage('https://images.unsplash.com/photo-1545341122-ded434ec92ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80')
+  
+}
+
+function second() {
+  document.body.style.backgroundColor = '#634832'
+  changeImage('https://images.unsplash.com/photo-1547254905-f38a43abfc0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80')
+}
+
+function third() {
+  document.body.style.backgroundColor = '#967259'
+  changeImage('https://images.unsplash.com/photo-1522120193639-2cd0bbe2979b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80')
+}
+
+function fourth() {
+  document.body.style.backgroundColor = '#dbc1ac'
+  changeImage('https://images.unsplash.com/photo-1514354900327-3e3f538a755c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80')
+}
+
+function fifth() {
+  document.body.style.backgroundColor = '#ece0d1'
+  changeImage('https://images.unsplash.com/photo-1498804103079-a6351b050096?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80')
+}
--- log.md
+++ log.md
@@ -1,2 +1,15 @@
 # Day 1 
 
+I made a simple background color picker to show cofffee colors! It also changes images once you clicked! I used onclick event listener instead of addEventListener. Personally, I dont use onclick event listener function but, I am curious what are the differences between onclick and addEventListener. 
+
+Diffenences: 
+| type              | addEventListener               | onclick                   |
+| ----------------- | ------------------------------ | ------------------------- |
+| number of event   | many events                    | single event              |
+| event propagation | can control with 3rd arguement | cant control              |
+| how to add        | `<script>` or  JS file         | HTML attribute or JS file |
+| compability       | Doesn't work on older IE       | works in all brwosers     |
+
+source: [Difference between addEventListenter and onclick in JavaScript](https://www.geeksforgeeks.org/difference-between-addeventlistener-and-onclick-in-javascript/)
+
+[Coffee background picker]()
